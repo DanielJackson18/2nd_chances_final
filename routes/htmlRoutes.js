786c75a8497a21ac51ab68f8e787e8203ce9f9f8
Daniel Jackson <officialdanieljackson@gmail.com>
2019-08-08 20:41:04 -0700

    gg

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,35 @@
+var db = require("../models");
+
+var path = require("path");
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/index.html'))
+  });
+
+  app.get("/register", function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/inputForm.html'))
+  });
+
+  app.get("/gallery", function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/gallery.html'))
+  });
+
+  app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/login.html'))
+  });
+
+  app.get("/signup", function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/register.html'))
+  });
+
+  app.get("/viewpets", function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/viewpets.html'))
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
